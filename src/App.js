235706b39fa0acc7bd518ed2854660cded52673a
ElevatedPeace <99437895+ElevatedPeace@users.noreply.github.com>
2023-04-26 21:31:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import CreateTask from "./CreateTasks";
+import Task from "./Tasks";
 
-function App() {
+const todoList = [
+  {
+    title: "Grab some Pizza",
+    completed: true,
+  },
+  {
+    title: "Eat lunch",
+    completed: true,
+  },
+  {
+    title: "Surf the Internet",
+    completed: false,
+  },
+];
+
+function Todo() {
+  const [tasks, setTasks] = useState(todoList);
+  const [TasksToDo, setTasksToDo] = useState(0);
+
+  useEffect(() => {
+    setTasksToDo(tasks.filter((task) => !task.completed).length);
+  }, [tasks]);
+
+  const addTask = (title) => {
+    const newTasks = [...tasks, { title, completed: false }];
+    setTasks(newTasks);
+  };
+
+  const completeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = true;
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-container">
+      <div className="header">Pending tasks ({TasksToDo})</div>
+      <div className="tasks">
+        {tasks.map((task, index) => (
+          <Task
+            task={task}
+            index={index}
+            completeTask={completeTask}
+            removeTask={deleteTask}
+            key={index}
+          />
+        ))}
+      </div>
+      <div className="create-task">
+        <CreateTask addTask={addTask} />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Todo;
---
+++ src/Tasks.jsx
@@ -0,0 +1,15 @@
+function Task({ task, index, completeTask, removeTask }) {
+  return (
+    <div
+      className="task"
+      style={{ textDecoration: task.completed ? "line-through" : "" }}
+    >
+      {task.title}
+      <button style={{ background: "red" }} onClick={() => removeTask(index)}>
+        X
+      </button>
+      <button onClick={() => completeTask(index)}>Complete</button>
+    </div>
+  );
+}
+export default Task;
